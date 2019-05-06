3d271447bdecd0f809872955ed65cc7f50e92ec0
amelenty <ada.melentyeva@gmail.com>
2019-05-06 09:56:52 +0300

    init

---
+++ base.rkt
@@ -0,0 +1,52 @@
+#lang racket
+(require racket/class)
+
+; premise and conclusion are both lists
+(struct rule (premise conclusion))
+
+; empty symbol
+(define epsilon "")
+
+(define cf-grammar%
+  (class object%
+    (super-new)
+    (init-field [terminal-alphabet '()]
+                [non-terminal-alphabet '()]
+                [rules '()]
+                [start epsilon])
+
+    (define/public (get-start) start)
+
+    (define/public (get-rules) rules)                
+
+    (define/public (is-terminal x)
+      (index-of terminal-alphabet x))
+
+    (define/public (is-non-terminal x)
+      (index-of non-terminal-alphabet x))
+    )
+  )
+
+; terminal alphabet is n types of pairs/brackets: a list of pairs
+(define dyck-grammar%
+  (class cf-grammar%
+    (init terminal-pairs)
+    (define start "Start")                     
+    (define non-terminal-alphabet (list start))
+    (define rules
+            ; S -> epsilon
+      (cons (rule start (list epsilon))
+            ; S -> a_i S b_i S for every pair of brackets
+            (map (lambda (bracket-pair)
+                   (rule start (list (first bracket-pair) start (last bracket-pair) start)))
+                 terminal-pairs)
+            )
+      )
+    (super-new
+     [start start]
+     [terminal-alphabet (flatten terminal-pairs)]
+     [non-terminal-alphabet non-terminal-alphabet]
+     [rules rules])
+    (inherit is-terminal is-non-terminal)
+    )
+  )
---
+++ gui.rkt
@@ -0,0 +1,5 @@
+#lang racket/gui
+
+(define main-frame (new frame% [label "Example"]))
+
+(send main-frame show #t)
